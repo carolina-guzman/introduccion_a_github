a497cfc32c618c6ff3fed6bb5128c5580e5d95a6
Carolina Guzman <carolina.guzmanc@alumnos.udg.mx>
2020-09-16 13:23:16 -0500

    Capturar edad

--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 
 int main(){
     char n[100];
+    unsigned int e;
 
     printf("Hola mundo\n");
     printf("Nombre: ");
     fgets(n, sizeof(n), stdin);
     printf("Hola %s", n);
+
+    printf("Edad: ");
+    scanf("%u", &e);
+    printf("Edad proxima: %u", e+1);
 
     return 0;
 }
