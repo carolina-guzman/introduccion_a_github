14599c8d098c848a66f2751403469c152034e2b1
carolina-guzman <carolina.guzman5480@alumnos.udg.mx>
2020-09-21 22:03:50 -0500

    comentar el hola mundo

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
     char n[100];
     unsigned int e;
 
-    printf("Hola mundo\n");
+    //printf("Hola mundo\n");
     printf("Nombre: ");
     fgets(n, sizeof(n), stdin);
     printf("Hola %s", n);
